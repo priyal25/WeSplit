b3bb7c275eb8245d66f5c466339b280a8ff260eb
Priyal <priyal.j25@gmail.com>
2024-04-23 10:51:34 +0530

    Start with splitting app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeopleIndex = 0
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    @FocusState private var amountIsFocused: Bool
+
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeopleIndex + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeopleIndex) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.menu)
+                }
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: "USD"))
+                }
+            }.navigationTitle("We Split")
+            
+        }
     }
 }
 
